c0ae4e4b0c0f5f0c44d8eb133ba32451a64be9a7
Ananya Arayacharoenwong <ananya11962547@gmail.com>
2022-11-02 17:39:58 +0700

    Exercise4-borntodev

---
+++ Exercise4_Ananya_A.py
@@ -0,0 +1,9 @@
+FE = 82
+GB = 86.2
+ICS = 93.5
+CP = 95
+print("--- Your Score ---")
+print("Foundation English :",FE )
+print("General Business :",GB)
+print("Introduction to Computer Systems :",ICS)
+print("Computer Programming :",CP)
