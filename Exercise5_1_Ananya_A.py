d1950a2c6571ebacd5a999caaf1c4387e1494f01
Ananya Arayacharoenwong <ananya11962547@gmail.com>
2022-11-03 20:31:19 +0700

    Exercise5_1

---
+++ Exercise5_1_Ananya_A.py
@@ -0,0 +1,6 @@
+num1 = int(input("1st Number:"))
+num2 = int(input("2nd Number:"))
+print(num1,"+",num2,"=",num1+num2)
+print(num1,"-",num2,"=",num1-num2)
+print(num1,"*",num2,"=",num1*num2)
+print(num1,"/",num2,"=",num1/num2)
---
+++ Exercise5_2_Ananya_A.py
@@ -0,0 +1,4 @@
+s = int(input("Distance(km):"))
+t = int(input("Time(h):"))
+v = s/t
+print(v,"km/h")
