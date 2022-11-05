ba5f1bd0cf167b2eaad0d47ce6969d49c2e4d8d1
Ananya Arayacharoenwong <ananya11962547@gmail.com>
2022-11-05 12:14:12 +0700

    Lecture50

---
+++ Lecture50_Ananya_A.py
@@ -0,0 +1,15 @@
+def addNumber1(x,y):
+    print(x+y)
+addNumber1(100,25)
+
+def addNumber2(x,y):
+    print(x-y)
+addNumber2(100,25)
+
+def addNumber3(x,y):
+    print(x*y)
+addNumber3(100,25)
+
+def addNumber4(x,y):
+    print(x/y)
+addNumber4(100,25)
